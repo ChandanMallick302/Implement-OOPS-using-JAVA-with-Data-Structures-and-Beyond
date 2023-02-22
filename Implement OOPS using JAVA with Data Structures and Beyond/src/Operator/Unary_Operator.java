00a498a32e3f06e97e967991b3ff0a563e69e85e
ChandanMallick302 <chandanmallick302@gmail.com>
2023-02-22 18:08:48 +0530

    update 1

---
+++ Implement OOPS using JAVA with Data Structures and Beyond/src/Operator/Unary_Operator.java
@@ -0,0 +1,14 @@
+package Operator;
+
+public class Unary_Operator {
+
+	public static void main(String[] args) {
+
+		int x = 10;
+		int b = 40;
+		int sum = x + b;
+		System.out.println(sum);
+
+	}
+
+}
